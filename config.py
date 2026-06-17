f6569dd8fd6b8e9bbe06a3b5d78cc321ee2690b5
smarttreader904-blip <smarttreader904@gmail.com>
2026-06-17 11:46:51 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,88 @@
+# =========================
+# BOT CONFIG
+# =========================
+
+BOT_TOKEN = "8859975301:AAEqR6ut9U_Vh77slUhAj5RdbwQmt2OoTXk"
+
+# Main Admin
+ADMIN_ID = 8559227368
+
+# Admin List
+ADMINS = [
+    8559227368
+]
+
+
+# =========================
+# DEFAULT COUNTRIES
+# =========================
+
+DEFAULT_COUNTRIES = [
+    "Bangladesh",
+    "India",
+    "Pakistan",
+    "Indonesia",
+    "Malaysia",
+    "Others"
+]
+
+
+# =========================
+# PLATFORMS
+# =========================
+
+PLATFORMS = [
+    "Telegram",
+    "WhatsApp",
+    "Facebook",
+    "TikTok"
+]
+
+
+# =========================
+# TEXTS
+# =========================
+
+START_TEXT = """
+👋 Welcome to Method Manager Bot.
+
+Select a platform below.
+"""
+
+SUPPORT_TEXT = """
+🛠 Please send your problem.
+"""
+
+ADD_METHOD_TEXT = """
+➕ Please select a platform.
+"""
+
+SELECT_COUNTRY_TEXT = """
+🌍 Please select server/country.
+"""
+
+CUSTOM_COUNTRY_TEXT = """
+✏️ Please send country/server name.
+"""
+
+SEND_METHOD_TEXT = """
+📝 Please send your method.
+"""
+
+BROADCAST_TEXT = """
+📢 Please send message.
+"""
+
+REJECT_REASON_TEXT = """
+❌ Please write reject reason.
+"""
+
+REPLY_TEXT = """
+💬 Please send your reply.
+"""
+
+NEW_METHOD_NOTIFICATION = """
+✅ New Country & Method Added Successfully.
+
+Use /start to check.
+"""
